ba97ccdbded8e645fd97cb20913638528591aec2
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-03-22 17:18:53 +0530

    Add makeCacheMatrix function with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function to create a special matrix object that can cache its inverse
+#
+#  returns a list of four functions:
+#  set(matrix)         - use to assign a matrix to be cached
+#  get()               - returns cached matrix
+#  setInverse(inverse) - use to cache the inverse of the matrix
+#  getInverse()        - returns the cached inverse
+#
+makeCacheMatrix <- function(matix = matrix()) {
+  mean <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+  set <- function (newMatrix) {
+    matrix <<- newMatrix
+    mean <<- NULL
+  }
+  get <- function () {
+    matrix
+  }
+  setInverse <- function (newMean) {
+    mean <<- newMean
+  }
+  getInverse <- function () {
+    mean
+  }
 
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
